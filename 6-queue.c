9f8e6bbfc64fbafdd43c958cccb056907970035c
anikethhebbar <76802363+anikethhebbar@users.noreply.github.com>
2022-01-20 13:21:08 +0530

    added circular queue program

---
+++ 6-queue.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define QSIZE 4
+char q[5], item;
+int r = -1, f = 0, count = 0;
+void insert()
+{
+    if (count == QSIZE)
+    {
+        printf("Queue Overflow\n");
+        return;
+    }
+    r = (r + 1) % QSIZE;
+    q[r] = item;
+    count++;
+}
+void del()
+{
+    if (count == 0)
+    {
+        printf("Queue Underflow\n");
+        return;
+    }
+    printf("The item deleted is: %c\n", q[f]);
+    f = (f + 1) % QSIZE;
+    count--;
+}
+void display(int front)
+{
+    int i;
+    if (count == 0)
+    {
+        printf("Queue is Empty\n");
+        return;
+    }
+    printf("Contents of the queue\n");
+    for (i = 1; i <= count; i++)
+    {
+        printf("%c\n", q[front]);
+        front = (front + 1) % QSIZE;
+    }
+}
+int main()
+{
+    int choice;
+    do
+    {
+        printf("Circular Queue Operations\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display\n");
+        printf("4. Quit\n");
+        printf("Enter your choice:\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the item to be inserted\n");
+            scanf("%s", &item);
+            insert();
+            break;
+        case 2:
+            del();
+            break;
+        case 3:
+            display(f);
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    } while (choice != 4);
+    return 0;
+}
